f3a27ee4420fb16a52ebd711a7cccc2c157157e9
julian2308 <julian.d.alvarado23@gmail.com>
2020-06-04 18:40:25 -0500

    Se crearon JAVADOCS para la funciones de área de un círculo, área de una esfera. Se crearon los JAVA class "WhileLoop" y "DoWhileLoop"

--- src/Functions.java
+++ src/Functions.java
@@ -16,6 +16,9 @@ public class Functions {
 
         System.out.println("PESOS A DOLARES: " + converToDollar(1000, "COP"));
 
+        System.out.println("El área de su esfera es: " + sphereArea(4));
+
+        System.out.println("El radio del círculo es:" + circleArea(5));
 
 
 
@@ -24,18 +27,37 @@ public class Functions {
 
 
     }
-
+    /**
+     * Descripción: Función que dando el radio de un círculo nos da su área
+     *
+     * @param r Es el radio del círculo del cúal queremos obtener su área
+     * @return circleArea devuelve el área del círculo
+     * {@code ¡Batman was here!}
+     * @exception double-5 La única excepción es el número 5
+     * */
     public static double circleArea(double r){
         return Math.PI * Math.pow(r,2);
     }
+    /**
+     * Descripción: Función que dando el radio de una esfera, nos calcula su área
+     *
+     * @param r Es el radio de la esfera la cual queremos obtener su área
+     * @return sphereArea devuelve el área de la esfera
+     * */
     public static double sphereArea (double r){
         return 4 * Math.PI * Math.pow(r,2);
     }
+    /**
+     * Descripción: Función que dando el radio de una esfera, nos da el volumen de esta
+     *
+     * @param r Es el radio de la esfera la cual queremos obtener su volumen
+     * @return spehreVolumen Devuelve el volumen de la esfera
+     * */
     public static double sphereVolumen (double r){
         return (4/3)*Math.PI*Math.pow(r,3);
     }
     /**
-     * Descripción: Función qie especificando su moneda convierte una cantidad de dinero a dólares
+     * Descripción: Función que especificando su moneda convierte una cantidad de dinero a dólares
      *
      * @param quantity cantidad de dinero
      * @param currency Tipo de moneda: Solo acepta MXN o COP
